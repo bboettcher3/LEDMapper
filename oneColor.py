4f8e83028cf13024e07c27dac1b06c7ad5627691
bboettcher3 <bradyboettcher@gmail.com>
2022-08-05 09:30:41 -0400

    adds existing files

---
+++ oneColor.py
@@ -0,0 +1,146 @@
+from enum import Enum
+import argparse
+import time
+from rpi_ws281x import PixelStrip, Color
+import sys, pygame, pygame.midi
+
+# LED strip configuration:
+LED_COUNT = 150        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 200  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+class LightPattern(Enum):
+    wipe = 1
+    theaterChase = 2
+    wheel = 3
+    rainbow = 4
+    rainbowCycle = 5
+    theaterChaseRainbow = 6
+
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        if (wait_ms != 0): 
+                strip.show()
+       		time.sleep(wait_ms / 1000.0)
+    strip.show()
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, color)
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, 0)
+
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel(
+                (int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+
+def theaterChaseRainbow(strip, wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, wheel((i + j) % 255))
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, 0)
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    #parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    curMode = 0
+
+    # set up pygame
+    pygame.init()
+    pygame.midi.init()
+
+    # list all midi devices
+    for x in range( 0, pygame.midi.get_count() ):
+        print(pygame.midi.get_device_info(x))
+
+    # open the first non-internal MIDI internal device
+    inp = pygame.midi.Input(3)
+
+    try:
+        while True:
+            if inp.poll():
+                # no way to find number of messages in queue
+                # so we just specify a high max value
+                print(inp.read(1000))
+            time.sleep(50.0/1000)
+            #print('Color wipe animations.')
+            #colorWipe(strip, Color(255, 0, 0), 0)  # Red wipe
+            #time.sleep(10)
+	    #colorWipe(strip, Color(0, 255, 0), 0)  # Green wipe
+            #time.sleep(10) 
+	    #colorWipe(strip, Color(0, 0, 255), 0)  # Blue wipe
+	    #time.sleep(10)
+	    #colorWipe(strip, Color(128, 128, 0), 0)
+            #time.sleep(10)
+	    #colorWipe(strip, Color(0, 128, 128), 0)
+	    #time.sleep(10)
+	    #colorWipe(strip, Color(128, 0, 128), 0)
+	    #time.sleep(10)
+            #print('Theater chase animations.')
+            #theaterChase(strip, Color(127, 127, 127))  # White theater chase
+            #theaterChase(strip, Color(127, 0, 0))  # Red theater chase
+            #theaterChase(strip, Color(0, 0, 127))  # Blue theater chase
+            #print('Rainbow animations.')
+            #rainbow(strip)
+            #rainbowCycle(strip)
+            #theaterChaseRainbow(strip)
+
+    except KeyboardInterrupt:
+        colorWipe(strip, Color(0, 0, 0), 10)
+
